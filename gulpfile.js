145634bc1bd075000445356d6e3e3d27234da1da
jody tate <jody@josephtate.com>
2016-07-05 17:05:36 -0700

    change to const

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var browserSync = require('browser-sync');
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var reload = browserSync.reload;
+const browserSync = require('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const reload = browserSync.reload;
 
 gulp.task('sass', function () {
   gulp.src('./styles/*.scss')
